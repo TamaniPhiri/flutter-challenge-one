0307762d8a6445b523e733eaedf265f213a74a49
tamani phiri <tamanigabriel0@gmail.com>
2023-10-05 00:17:02 +0200

    changes

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -10,6 +10,6 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }

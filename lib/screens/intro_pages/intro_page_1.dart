41acfb2871f747de6eb5e614d2796a12225795e9
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 16:28:06 +0200

    changes

--- lib/screens/intro_pages/intro_page_1.dart
+++ lib/screens/intro_pages/intro_page_1.dart
@@ -9,14 +9,17 @@ class IntroPage1 extends StatelessWidget {
     return Container(
       color: Colors.blue,
       child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          "Having a hard time with projects",
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        ),
-        Lottie.network(
-            'https://lottie.host/b8d874a2-78a0-4903-8a8a-4465e1681196/Y81ySWlPbB.json')
-      ])),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+            const Text(
+              "Having a hard time with projects",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Lottie.network(
+                'https://lottie.host/b8d874a2-78a0-4903-8a8a-4465e1681196/Y81ySWlPbB.json')
+          ])),
     );
   }
 }

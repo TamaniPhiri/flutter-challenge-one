c7d27ad84d36028c5fbd39dd3aedb8ee77eb5a4c
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 15:02:29 +0200

    changes

--- lib/screens/intro_pages/inro_page_one.dart
+++ lib/screens/intro_pages/inro_page_one.dart
@@ -5,6 +5,8 @@ class IntroPage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Colors.blue,
+    );
   }
 }

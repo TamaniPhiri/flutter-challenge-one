2b13eeace65d2ae3e30f98fed0ea76723cd36761
tamani phiri <tamanigabriel0@gmail.com>
2023-10-05 00:14:57 +0200

    changes

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -55,8 +55,8 @@ class _LoginPageState extends State<LoginPage> {
               decoration: const BoxDecoration(
                   color: Colors.white60,
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50))),
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Column(

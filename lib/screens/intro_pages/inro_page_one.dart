85bf7524f7d253d4e517c28e28612c00fe4fb0db
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 14:57:22 +0200

    changes

---
+++ lib/screens/intro_pages/inro_page_one.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class IntroPageOne extends StatefulWidget {
+  const IntroPageOne({super.key});
+
+  @override
+  State<IntroPageOne> createState() => _IntroPageOneState();
+}
+
+class _IntroPageOneState extends State<IntroPageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}

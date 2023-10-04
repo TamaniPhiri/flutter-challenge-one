8ebf258650a2e3dd7ecc6891c740a38c17bdf54b
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 15:59:55 +0200

    changes

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -10,6 +10,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+    );
   }
 }

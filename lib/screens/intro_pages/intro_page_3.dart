b1177c11ece74bb40e59aabc4ab4a1d1ef69ec33
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 17:04:06 +0200

    changes

--- lib/screens/intro_pages/intro_page_3.dart
+++ lib/screens/intro_pages/intro_page_3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage3 extends StatelessWidget {
   const IntroPage3({super.key});
@@ -6,12 +7,22 @@ class IntroPage3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color.fromARGB(255, 4, 64, 114),
-      child: const Center(
-          child: Text(
-        "You are just at the right place",
-        style: TextStyle(color: Colors.white),
-      )),
-    );
+        color: const Color.fromARGB(255, 4, 64, 114),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Well, look no further",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              SizedBox(
+                height: 400,
+                width: 400,
+                child: Lottie.network(
+                    'https://lottie.host/945d9cfc-0712-4e43-bcb5-5782c2875613/zFGutDUU2R.json',
+                    fit: BoxFit.cover),
+              ),
+            ]));
   }
 }

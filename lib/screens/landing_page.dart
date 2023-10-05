c0de38b9e09e8b8b7efcb8ecf647ff19f3fee1b9
tamani phiri <tamanigabriel0@gmail.com>
2023-10-05 09:33:56 +0200

    changes

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -10,82 +10,11 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   int _selectedIndex = 0;
+  List<Widget> tabItems = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 300,
-            floating: true,
-            pinned: true,
-            leading: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.person),
-                  ),
-                ],
-              ),
-            ),
-            title: Align(
-              alignment: Alignment.centerRight,
-              child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.notification_add_rounded)),
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 80,
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white60,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20.0, vertical: 8),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none, hintText: "Search"),
-                          ),
-                        ))
-                  ],
-                ),
-              ),
-              title: const Text("he"),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Container(
-                  color: Colors.purple,
-                  height: 400,
-                )
-              ],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Container(
-                  color: Colors.purple,
-                  height: 400,
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+      body: tabItems[_selectedIndex],
       bottomNavigationBar: FlashyTabBar(
         selectedIndex: _selectedIndex,
         onItemSelected: (index) => setState(() {

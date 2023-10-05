abffe5f6ec5a7d82030c08b277f4a3825b34a319
tamani phiri <tamanigabriel0@gmail.com>
2023-10-05 11:06:17 +0200

    changes

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -4,7 +4,6 @@ import 'package:challenge_ui_one/screens/welcome_page.dart';
 import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -33,25 +32,22 @@ class _LandingPageState extends State<LandingPage> {
         items: [
           FlashyTabBarItem(
               icon: const Icon(CupertinoIcons.home),
-              title: Text(
+              title: const Text(
                 "Home",
-                style: GoogleFonts.poppins(),
               ),
               inactiveColor: Colors.white,
               activeColor: Colors.white),
           FlashyTabBarItem(
               icon: const Icon(CupertinoIcons.today),
-              title: Text(
+              title: const Text(
                 "Tasks",
-                style: GoogleFonts.poppins(),
               ),
               inactiveColor: Colors.white,
               activeColor: Colors.white),
           FlashyTabBarItem(
               icon: const Icon(Icons.schedule),
-              title: Text(
+              title: const Text(
                 "Schedule",
-                style: GoogleFonts.poppins(),
               ),
               inactiveColor: Colors.white,
               activeColor: Colors.white),

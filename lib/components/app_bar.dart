bc3baf57e9bc65fc47b7185528d4e51f8cb1d1e9
tamani phiri <tamanigabriel0@gmail.com>
2023-10-05 15:32:53 +0200

    changes

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatefulWidget {
+  const MyAppBar({super.key});
+
+  @override
+  State<MyAppBar> createState() => _MyAppBarState();
+}
+
+class _MyAppBarState extends State<MyAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

67b43a45f8e723f57fad32624a496327971234dd
tamani phiri <tamanigabriel0@gmail.com>
2023-10-05 10:51:53 +0200

    changes

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:challenge_ui_one/screens/schedule_page.dart';
 import 'package:challenge_ui_one/screens/task_page.dart';
 import 'package:challenge_ui_one/screens/welcome_page.dart';
 import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
@@ -30,17 +31,20 @@ class _LandingPageState extends State<LandingPage> {
         }),
         items: [
           FlashyTabBarItem(
-              icon: const Icon(Icons.home),
+              icon: const Icon(CupertinoIcons.home),
               title: const Text("Home"),
+              inactiveColor: Colors.white,
               activeColor: Colors.white),
           FlashyTabBarItem(
-              icon: const Icon(Icons.task),
+              icon: const Icon(CupertinoIcons.tag_solid),
               title: const Text("Tasks"),
+              inactiveColor: Colors.white,
               activeColor: Colors.white),
           FlashyTabBarItem(
               icon: const Icon(Icons.schedule),
               title: const Text("Schedule"),
-              activeColor: Colors.white)
+              inactiveColor: Colors.white,
+              activeColor: Colors.white),
         ],
         iconSize: 30,
         showElevation: true,

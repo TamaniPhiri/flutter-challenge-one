f62162c5657dac5b0828e145b2bd3bef186a4dec
tamani phiri <tamanigabriel0@gmail.com>
2023-10-05 15:34:47 +0200

    changes

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../utils/categories_filter.dart';
 
 class MyAppBar extends StatefulWidget {
   const MyAppBar({super.key});
@@ -10,6 +13,78 @@ class MyAppBar extends StatefulWidget {
 class _MyAppBarState extends State<MyAppBar> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SliverAppBar(
+      backgroundColor: Color.fromARGB(255, 4, 64, 114),
+      expandedHeight: 300,
+      floating: false,
+      pinned: true,
+      leading: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.person),
+        ),
+      ),
+      title: Align(
+        alignment: Alignment.centerRight,
+        child: IconButton(
+            onPressed: () {}, icon: const Icon(Icons.notification_add_rounded)),
+      ),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Color.fromARGB(255, 4, 64, 114),
+            Colors.blue,
+          ])),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              const Row(
+                children: [
+                  Text(
+                    "Hello",
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Gabby",
+                    style: TextStyle(fontSize: 32, color: Colors.white60),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // Search Input
+              CupertinoTextField(
+                placeholder: "Search",
+                padding: const EdgeInsets.all(20),
+                suffix: IconButton(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    onPressed: () {},
+                    icon: const Icon(CupertinoIcons.search)),
+                decoration: BoxDecoration(
+                  color: Colors.white60,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const CategoryFilter()
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,5 +1,3 @@
-import 'package:challenge_ui_one/utils/categories_filter.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -14,80 +12,6 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        SliverAppBar(
-          backgroundColor: Color.fromARGB(255, 4, 64, 114),
-          expandedHeight: 300,
-          floating: false,
-          pinned: true,
-          leading: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.person),
-            ),
-          ),
-          title: Align(
-            alignment: Alignment.centerRight,
-            child: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.notification_add_rounded)),
-          ),
-          flexibleSpace: FlexibleSpaceBar(
-            background: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-                Color.fromARGB(255, 4, 64, 114),
-                Colors.blue,
-              ])),
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  const Row(
-                    children: [
-                      Text(
-                        "Hello",
-                        style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Gabby",
-                        style: TextStyle(fontSize: 32, color: Colors.white60),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  // Search Input
-                  CupertinoTextField(
-                    placeholder: "Search",
-                    padding: const EdgeInsets.all(20),
-                    suffix: IconButton(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        onPressed: () {},
-                        icon: const Icon(CupertinoIcons.search)),
-                    decoration: BoxDecoration(
-                      color: Colors.white60,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const CategoryFilter()
-                ],
-              ),
-            ),
-          ),
-        ),
         SliverToBoxAdapter(
           child: Column(
             children: [
